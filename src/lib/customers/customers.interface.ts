8a8e94f326ecae24aeef71c32b3251be8f6d1f27
Damilare Anjorin <damilareanjorin1@gmail.com>
2023-12-26 15:47:56 +0100

    chore: update customers

--- src/lib/customers/customers.interface.ts
+++ src/lib/customers/customers.interface.ts
@@ -8,6 +8,42 @@ export interface CreateCustomerRequestPayload {
 
 export type CreateBulkCustomerRequestPayload = CreateCustomerRequestPayload[]
 
+export interface UpdateCustomerRequestPayload {
+  first_name?: string
+  last_name?: string
+  phone_no?: string
+}
+
+export interface CustomerQueryOptions {
+  page?: number
+  per_page?: number
+  query?: string
+}
+
+export interface CustomerPayload {
+  id: string
+  first_name: string | null
+  last_name: string | null
+  email: string
+  phone_no: string | null
+  providers?: string | null
+  customer_created_at?: string | null | Date
+  updated_at?: string | null | Date
+  last_interaction?: string | null | Date
+}
+
+export interface CustomersResponsePayload {
+  data: CustomerPayload[]
+  meta: {
+    per_page: number
+    total_pages?: number
+    total: number
+  }
+}
+
 export interface ICustomers {
-  create(data: CreateCustomerRequestPayload): Promise<AxiosResponse>
+  create(customer: CreateCustomerRequestPayload): Promise<AxiosResponse>
+  bulkCreate(customers: CreateBulkCustomerRequestPayload): Promise<AxiosResponse>
+  update(email: string, customer: UpdateCustomerRequestPayload): Promise<AxiosResponse>
+  all(query?: CustomerQueryOptions): Promise<AxiosResponse<CustomersResponsePayload>>
 }
--- src/lib/customers/customers.ts
+++ src/lib/customers/customers.ts
@@ -1,21 +1,32 @@
 import { Http } from '../bunce.interface'
-import {
+import type {
   CreateBulkCustomerRequestPayload,
   CreateCustomerRequestPayload,
+  CustomerQueryOptions,
+  CustomersResponsePayload,
   ICustomers,
+  UpdateCustomerRequestPayload,
 } from './customers.interface'
-import { AxiosResponse } from 'axios'
+import type { AxiosResponse } from 'axios'
 
 export class Customers extends Http implements ICustomers {
   async create(customer: CreateCustomerRequestPayload): Promise<AxiosResponse> {
-    return await this.http.post(`/customers`, {
-      customer,
-    })
+    return await this.http.post(`/customers`, customer)
   }
 
   async bulkCreate(customers: CreateBulkCustomerRequestPayload): Promise<AxiosResponse> {
-    return await this.http.post(`/customers/bulk`, {
-      customers,
-    })
+    return await this.http.post(`/customers/bulk`, customers)
+  }
+
+  async update(email: string, customer: UpdateCustomerRequestPayload): Promise<AxiosResponse> {
+    return await this.http.put(`/customers/${email}`, customer)
+  }
+
+  async all(params?: CustomerQueryOptions): Promise<AxiosResponse<CustomersResponsePayload>> {
+    const toQuery = Object.keys(params ?? {})
+      .map((key) => `${key}=${(params as any)[key]}`)
+      .join('&')
+
+    return await this.http.get(`/customers?${toQuery}`)
   }
 }
